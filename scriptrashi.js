03d483c494eb8c297471b2e9dd54d3ca36691157
zamit00 <zamit00@gmail.com>
2024-11-24 17:29:32 +0200

    Update scriptrashi.js

--- scriptrashi.js
+++ scriptrashi.js
@@ -1,63 +1,85 @@
 
 //פונקציה לפתיחה וסגירה של המבורגר//
 
-function homeclick() {
-    let x = document.getElementById("mynav");
-        if( x.className!=="mynav"){x.className="mynav"};
-    }
 
-
-    function hamburger(){
-      var nav=document.getElementById('mynav');
-    if (nav.className === "mynav") { nav.className  += " responsive";} else {nav.className = "mynav";}
-  };
-  // פונקציה שתופעל כשגודל המסך משתנה
+window.addEventListener('resize', checkScreenSize);
+checkScreenSize();
 function checkScreenSize() {
-    if(window.innerWidth >390 && window.innerWidth <410 ){console.log(window.innerWidth);}
-    if(window.innerWidth >570 && window.innerWidth <580 ){console.log(window.innerWidth);}
-    if(window.innerWidth >763 && window.innerWidth <772 ){console.log(window.innerWidth);}
-    if(window.innerWidth >1020 && window.innerWidth <1028 ){console.log(window.innerWidth);}
     handleLargeScreen(); 
   } 
+ function handleLargeScreen() {
+	const alla = document.querySelectorAll('a.anav');
+	alla.forEach(function(menu) {
+	if(window.innerWidth>768){
+		if (menu.id!=="icon" && menu.id!=="home"){menu.display= "inline-block"}   
+	   else{if (menu.id==="icon"){menu.display= "none"}  }
+	   }
+	   
+	else{ if (menu.id ==="icon"){menu.display= "inline-block"}
+			else{if (menu.id!=="home"){menu.display= "none"}  }
+		}
+ });}
+  
+
+
+  //  function hamburger(){
+    
+//		document.getElementById("nav").style.display="flex";
+//		document.getElementById("nav").style.flexDirection="column";
+		
+//	   const alla = document.querySelectorAll('a.anav');
+//	   alla.forEach(function(menu) {
+        // הסתרת כל התפריטים
+ //            menu.style.display = 'none';
+//        });  
+//	   x.className==="mynav responsive";
+		
+//  };
+  // פונקציה שתופעל כשגודל המסך משתנה
+
 
 
 // הפונקציות שיתבצעו לפי הגודל
-function handleLargeScreen() {
-    var x = document.getElementById("mynav");
-   if(window.innerWidth>767){x.className==="mynav";}else{x.className==="mynav responsive"}
-  }
 
 // הוספת מאזין לשינוי בגודל המסך
-window.addEventListener('resize', checkScreenSize);
+
 
 // קריאה ראשונית לפונקציה כאשר הדף נטען
-checkScreenSize();
-function ulop(element) {
+
+/*function ulop(element) {
+	
     const allMenus = document.querySelectorAll('a.anav > ul');
 
     // לולאה שסוגרת את כל התפריטים האחרים
     allMenus.forEach(function(menu) {
         // הסתרת כל התפריטים
-        if (menu !== element.querySelector('ul')) {
+       if (menu !== element.querySelector('ul')) {
             menu.style.display = 'none';
         }
     });
 
-
+	element.style.display="block";
+	
     // גישה ל-<ul> מתוך האלמנט <a> שלחץ עליו
-    const ul = element.querySelector('ul');
+   const ul = element.querySelector('ul');
 
     // בדוק אם יש <ul> ופעול עליו
-    if (ul) {
+   if (ul) {
         // כאן אני מבצע פעולה על ה-ul, כמו הצגה/הסתרה של התפריט
-        if (ul.style.display === 'none' || ul.style.display ==="") {
-            if(window.innerWidth >767){ul.style.display = 'block';} 
-            if(window.innerWidth <768){ul.style.display = 'flex';}
+       if (ul.style.display === 'none' || ul.style.display ==="") {
+            if(window.innerWidth >767){
+				ul.style.display = 'block';} 
+            if(window.innerWidth <768){
+				ul.style.display = 'flex';
+				ul.flexDirection="column";
+				}
         } else {
-            ul.style.display = 'none';  // הסתרת ה-ul
+			
+           ul.style.display = 'none';  // הסתרת ה-ul
         }
-    }
-}
+}}*/
+
+
 /*function showMenu(element) {
     const ul = element.querySelector('ul');
     if (ul && (ul.style.display === 'none' || ul.style.display === '')) {
