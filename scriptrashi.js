c0daf182f5c5d3e7b092c648af5ce298dd50a655
zamit00 <zamit00@gmail.com>
2024-11-27 15:36:28 +0200

    Update scriptrashi.js

--- scriptrashi.js
+++ scriptrashi.js
@@ -28,36 +28,17 @@ function myFunction(x) {
         
         if (butt.className === "dropbtn") {
           butt.className='dropbtna';
-
-        } else {
+        } 
+        else {
           butt.className='dropbtn';
-        }
-      
+
+          for(let i=1;i<=3;i++){
+            var taf= document.getElementById(`dropdown-content${i}`);
+                taf.className='dropdown-content';
+           }
+         }
+            
     }
   }
-  /*
-  const allMenus = x.querySelectorAll('div');
-  allMenus.forEach(menu => {
-    if (x.id==='btn1')
-    menu.style.display = 'none';});
-
-  if (x.id==='btn1'){
-
-  
-  var taf = document.getElementById("dropdown-content1");
-  if (taf.style.display === "" || taf.style.display ==='none') {
-    taf.style.display='block';
-  } else {
-    taf.style.display ='none';
-  }
-}
-if (x.id==='btn2'){
-
-  
-  var taf = document.getElementById("dropdown-content2");
-  if (taf.style.display === "" || taf.style.display ==='none') {
-    taf.style.display='block';
-  } else {
-    taf.style.display ='none';
-  }
-}*/
+      
+ 
