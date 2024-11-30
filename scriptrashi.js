713c52424fed145e266c9abfcace945cb9b576d9
zamit00 <zamit00@gmail.com>
2024-11-30 22:04:31 +0200

    Update scriptrashi.js

--- scriptrashi.js
+++ scriptrashi.js
@@ -1,3 +1,4 @@
+
 function myFunction() {
 
   const icon = document.getElementById("icon");
@@ -27,15 +28,29 @@ if(menu.className ==='dropdown-content'){
  else{menu.className='dropdown-content'}
   }
   
-
-function home(){
-  const icon = document.getElementById("icon");
-   icon.innerHTML = "☰";
-  var mynav= document.getElementById("myTopnav");
-   mynav.className = "topnav";
-   }
-
 /*
+function humburger(){
+  var mynav= document.getElementById("myTopnav");
+      for(let i=1;i<=4;i++){
+  var butt= document.getElementById(`drd${i}`);     
+      if (butt.className === "dropbtn") {
+        butt.className='dropbtna';
+                } 
+      else {
+        butt.className='dropbtn';
+      
+
+        for(let i=1;i<=3;i++){
+          var taf= document.getElementById(`dropdown-content${i}`);
+          taf.className='dropdown-content';
+         }
+          
+
+       }
+          
+  }
+}
+
 function iconx(){
 
    for(let i=1;i<=4;i++){
